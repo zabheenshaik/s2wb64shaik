45cebae43ef655cf79a2b879b41ec3ca7b7476be
zabheenshaik <98142661+zabheenshaik@users.noreply.github.com>
2022-04-06 19:08:27 -0500

    index.pug

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pizzaRouter = require('./routes/pizza');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pizza', pizzaRouter);
 app.use('/addmods', addmodsRouter);
-
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
